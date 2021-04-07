e613f11ed0ba440ea6184552b20529be1476e7bd
Vanille-N <neven.villani@gmail.com>
2021-04-07 13:47:01 +0200

    greek

---
+++ src/greek.ml
@@ -0,0 +1,89 @@
+let domain n =
+  (* For each column i, row j and symbol k,
+   * we have one variable x_i,j,k indicating that symbol k is there. *)
+  let lat = Dimacs.(make (n**n**n**o)) in
+  let grk = Dimacs.(make (n**n**n**o)) in
+  (lat, grk)
+
+let problem n =
+  let (lat, grk) = domain n in
+  (* For all i,j no more than one symbol at i,j for both lat and grk. *)
+  (* also, no more than one [line|column] for each symbol *)
+  for i = 0 to n-1 do
+    for j = 0 to n-1 do
+      for k = 0 to n-1 do
+        for k' = k+1 to n-1 do
+          Dimacs.(add_clause [not lat.(i).(j).(k); not lat.(i).(j).(k')]) ;
+          Dimacs.(add_clause [not grk.(i).(j).(k); not grk.(i).(j).(k')])
+        done ;
+        for i' = i+1 to n-1 do
+          Dimacs.(add_clause [not lat.(i).(j).(k); not lat.(i').(j).(k)]) ;
+          Dimacs.(add_clause [not grk.(i).(j).(k); not grk.(i').(j).(k)])
+        done;
+        for j' = j+1 to n-1 do
+          Dimacs.(add_clause [not lat.(i).(j).(k); not lat.(i).(j').(k)]) ;
+          Dimacs.(add_clause [not grk.(i).(j).(k); not grk.(i).(j').(k)])
+        done
+      done
+    done
+  done ;
+  (* For all i,j there is a k such that x_i,j,k.
+   * For all i,k there is a k such that x_i,j,k.
+   * For all j,k there is a k such that x_i,j,k. *)
+  for a = 0 to n-1 do
+    for b = 0 to n-1 do
+      Dimacs.(add_clause (bigor n (fun c -> lat.(a).(b).(c)))) ;
+      Dimacs.(add_clause (bigor n (fun c -> lat.(a).(c).(b)))) ;
+      Dimacs.(add_clause (bigor n (fun c -> lat.(c).(a).(b)))) ;
+      Dimacs.(add_clause (bigor n (fun c -> grk.(a).(b).(c)))) ;
+      Dimacs.(add_clause (bigor n (fun c -> grk.(a).(c).(b)))) ;
+      Dimacs.(add_clause (bigor n (fun c -> grk.(c).(a).(b))))
+    done
+  done ;
+  (* now express the fact that there is no duplicate pairing *)
+  for i = 0 to n-1 do
+    for i' = 0 to n-1 do
+      for j = 0 to n-1 do
+        for j' = 0 to n-1 do
+          if (i,j) <> (i',j') then (
+            for k = 0 to n-1 do
+              for k' = 0 to n-1 do
+                Dimacs.(add_clause [
+                    not lat.(i).(j).(k);
+                    not lat.(i').(j').(k);
+                    not grk.(i).(j).(k');
+                    not grk.(i').(j').(k')
+                ])
+              done
+            done
+          )
+        done
+      done
+    done
+  done
+(* *)
+
+let solution n =
+  let (lat, grk) = domain n in
+  let m = Dimacs.read_model (open_in "output.sat") in
+  for i = 0 to n-1 do
+    for j = 0 to n-1 do
+      Format.printf "[" ;
+      for k = 0 to n-1 do
+        if Dimacs.sat m lat.(i).(j).(k) then (
+          Format.printf "%c"
+            (char_of_int (k + int_of_char 'A'))
+        )
+      done ;
+      for k = 0 to n-1 do
+        if Dimacs.sat m grk.(i).(j).(k) then (
+          Format.printf "%c"
+            (char_of_int (k + int_of_char 'a'))
+        )     
+      done ;
+      Format.printf "]"
+    done ;
+    Format.printf "\n"
+  done
+
+let () = Dimacs.run_int ~problem ~solution
