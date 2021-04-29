aabd83be6d03b842315ae323efd5bb9bd200dfa2
Vanille-N <neven.villani@gmail.com>
2021-04-29 10:25:11 +0200

    type of a DLL

--- src/twl.ml
+++ src/twl.ml
@@ -76,6 +76,17 @@ module Model : MODEL = struct
         Format.fprintf chan "]"
 end
 
+module type DLL = sig
+    type ('elt, 'mk) t
+    val make : unit -> ('elt, 'mk) t
+    val insert : ('elt, _) t -> 'elt -> unit
+    val peek : ('elt, _) t -> 'elt option
+    val take : ('elt, _) t -> 'elt option
+    val set_mark : (_, 'mk) t -> 'mk option -> unit
+    val get_mark : (_, 'mk) t -> 'mk option
+    val rotate : ('elt, _) t -> unit
+end
+
 exception Conflict
 exception SAT
 exception Found of int
